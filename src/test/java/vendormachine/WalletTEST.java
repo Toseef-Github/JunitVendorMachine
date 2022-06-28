355580f1fe9ecd5270a15adadf4c6ab49cdf2722
Toseef-Github <105277464+Toseef-Github@users.noreply.github.com>
2022-06-28 17:55:15 +0100

    added wallet tests
    added wallet tests

--- src/test/java/vendormachine/WalletTEST.java
+++ src/test/java/vendormachine/WalletTEST.java
@@ -1,20 +1,25 @@
 package vendormachine;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 import vendormachine.users.util.Wallet;
 
 public class WalletTEST {
 	
-	/** 
-	 * Task 1:
-	 * 	Objective:
-	 * 		Test the Wallet class from:
-	 * 		'vendormachine.users.util.Wallet'
-	 * 
-	 * 		Goal: 80% code/line coverage
-	 * */
+	@Test
+	public void testStartCredit() {
+		Wallet wallet = new Wallet(1F);
+		assertEquals(wallet.getAllCredit(), 1L);
+	}
 	
-	// Hint: 
-	// Others Tests for other classes that use Wallet may impact code coverage
-	// Avoid relying on these tests, one test for one class; keep "Single Responsibility" in mind.
+	@Test
+	public void testAddCredit() {
+		Wallet wallet1 = new Wallet();
+		wallet1.addCredit(2f);
+		assertEquals(2.5f, wallet1.getAllCredit());
+	}
 	
-}
+	
+	}
