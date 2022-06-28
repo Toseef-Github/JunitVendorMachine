930b509453b7035ff376d2c8d2cb431c280cdbf7
Toseef-Github <105277464+Toseef-Github@users.noreply.github.com>
2022-06-29 00:12:39 +0100

    updated wallet tests
    updated wallet tests

--- src/test/java/vendormachine/WalletTEST.java
+++ src/test/java/vendormachine/WalletTEST.java
@@ -8,11 +8,14 @@ import vendormachine.users.util.Wallet;
 
 public class WalletTEST {
 	
+	public final Wallet wallet1 = new Wallet();
+	
 	@Test
 	public void testStartCredit() {
-		Wallet wallet = new Wallet(1F);
-		assertEquals(wallet.getAllCredit(), 1L);
+		Wallet wallet1 = new Wallet(1F);
+		assertEquals(wallet1.getAllCredit(), 1L);
 	}
+	
 	
 	@Test
 	public void testAddCredit() {
@@ -21,5 +24,22 @@ public class WalletTEST {
 		assertEquals(2.5f, wallet1.getAllCredit());
 	}
 	
+	@Test
+	public void testGetCredit() {
+		assertEquals(wallet1.getCredit(0.5F), 0.5F);
+	}
+
+	@Test
+	public void testGetTooMuchCredit() {
+		assertEquals(wallet1.getCredit(2F), 0);
+	}
+
+
+	@Test
+	public void testSetCredit() {
+		wallet1.setCredit(2F);
+		assertEquals(wallet1.getAllCredit(), 2F);
+	}
+
 	
 	}
